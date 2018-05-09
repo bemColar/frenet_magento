fe012db9db66014f3511dca2959be1c69b160be5
bemColar <lucas@bemcolar.com.br>
2018-05-09 18:46:14 -0300

    Update Akhilleus.php

--- app/code/community/LithiumSoftware/Akhilleus/Model/Carrier/Akhilleus.php
+++ app/code/community/LithiumSoftware/Akhilleus/Model/Carrier/Akhilleus.php
@@ -507,7 +507,7 @@ class LithiumSoftware_Akhilleus_Model_Carrier_Akhilleus
             break;
         }
 
-        $url    = $this->getConfigData('ws');
+        $url    = $this->getConfigData('url_ws');
         $client = new SoapClient($url, array("soap_version" => SOAP_1_1,"trace" => 1));
         //$orderId = Mage::getModel("sales/order")->getCollection()->getLastItem()->getIncrementId();
         $order = Mage::getModel('sales/order')->load($orderId);
